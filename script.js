cb17a045a9cf7d96896622c00c8f94244bdefbda
Rakesh <rakeshcse123@gmail.com>
2023-04-30 14:11:42 +0530

    Rakesh Team Habba

--- script.js
+++ script.js
@@ -27,3 +27,10 @@ function myFunction() {
 
 }
 
+document.getElementById("myBtn").onclick = function() {myFunction()};
+
+/* myFunction toggles between adding and removing the show class, which is used to hide and show the dropdown content */
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
