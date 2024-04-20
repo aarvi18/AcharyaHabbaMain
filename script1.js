4ea7541854f1020d60fc11b61241e70ec066dfb4
Mohithesh H U <mohithesh46@gmail.com>
2024-04-20 08:49:15 +0530

    updated on 20/04/2024 -- 08:48

--- script1.js
+++ script1.js
@@ -8,7 +8,7 @@ function updateCountdown () {
     const timeRemaining = targetDateTime - currentDate;
 
     if (timeRemaining <= 0) {
-        document.getElementById('countdown').innerHTML = "Welcome to Course by Sid";
+        document.getElementById('countdown').innerHTML = "Welcome to HABBA 2K24";
     } else {
 
         const days = Math.floor(timeRemaining / (1000*60*60*24));
